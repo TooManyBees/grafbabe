9dc7426134f55f508bd1df2a9ef926a5f48f511c
Jess Bees <hi@toomanybees.com>
2026-07-17 16:43:55 -0400

    be more explicit about the meaning of this constant

--- src/database.rs
+++ src/database.rs
@@ -156,8 +156,10 @@ pub fn store_snapshot(connection: &mut Connection, snapshot: &[MetricFamily]) ->
 pub fn prune_old_metrics(connection: &Connection) -> rusqlite::Result<usize> {
     let mut statement = connection.prepare(
         "DELETE FROM events
-        WHERE timestamp < (unixepoch('now') - 2592000);",
+        WHERE timestamp < (unixepoch('now') - ?);",
     )?;
 
-    statement.execute(())
+    const ONE_MONTH_MILLIS: i64 = 1000 * 60 * 60 * 24 * 30;
+
+    statement.execute((ONE_MONTH_MILLIS,))
 }
