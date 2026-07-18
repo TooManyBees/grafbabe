edfd941fa680e9ce9c7b259857d6b572d7bd5b4b
Jess Bees <hi@toomanybees.com>
2026-07-18 11:40:25 -0400

    Move seeding of db into its own function

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,30 @@ fn parse_prometheus<'a>(s: &'a str) -> Result<Vec<MetricFamily<'a>>, ParseError>
     parse_payload(s.as_bytes(), Format::Text(TextFormat::Prometheus)).collect()
 }
 
-fn main() {
-    let mut f = File::open("./prometheus.txt").unwrap();
+enum SeedError {
+    IO(std::io::Error),
+    Parse(ParseError),
+    DB(rusqlite::Error),
+}
+fn seed_database(connection: &mut Connection) -> Result<(), SeedError> {
+    let mut f = File::open("./prometheus.txt").map_err(SeedError::IO)?;
     let mut s = String::new();
-    f.read_to_string(&mut s).unwrap();
-    let metrics = parse_prometheus(&s).unwrap();
+    f.read_to_string(&mut s).map_err(SeedError::IO)?;
+    let metrics = parse_prometheus(&s).map_err(SeedError::Parse)?;
 
+    // for n in 0..(30 * 24 * 60) {
+    //     if n % 100 == 0 {
+    //         println!("{n}");
+    //     }
+        database::store_snapshot(connection, &metrics).map_err(SeedError::DB)?;
+    // }
+
+    Ok(())
+}
+
+fn main() {
     let mut connection = database::get_connection("./pview.db3").unwrap();
     database::init_database(&connection).unwrap();
 
-    database::store_snapshot(&mut connection, &metrics).unwrap();
+    seed_database(&mut connection).unwrap();
 }
